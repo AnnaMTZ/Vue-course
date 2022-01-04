52d443222ed8dc4b35f59a091929a090353c7169
Anna <52211360+AnnaMTZ@users.noreply.github.com>
2022-01-04 21:11:26 +0100

    A bit of a composition api

--- App.vue
+++ App.vue
@@ -1,77 +1,90 @@
 <template>
-  <div>
-    <the-header></the-header>
-    <!-- slotProps needs to be used with the router -->
-    <router-view v-slot="slotProps">
-      <!-- </transition> is used to add animations -->
-    <transition name="route" mode="out-in"> 
-      <!-- :is="slotProps.Component - will dynamically load the component set by the router-->
-      <component :is="slotProps.Component"></component>
-    </transition>
-    </router-view>
-  </div>
+  <section class="container">
+    <h2>{{ userName }}</h2>
+    <h3>{{ age }}</h3>
+    <button @click="setAge">Change Age</button>
+    <div>
+      <input type="text" placeholder="First Name" v-model="firstName" />
+      <input type="text" placeholder="Last Name" v-model="lastName" />
+    </div>
+  </section>
 </template>
 
 <script>
-import TheHeader from './components/layout/TheHeader.vue';
+import { ref, computed, watch } from 'vue';
 
 export default {
-  components: {
-    TheHeader
-  }, created() {
-    this.$store.dispatch('tryLogin');
-  }, 
-  computed: {
-    didAutoLogout() {
-      return this.$store.getters.didAutoLogout;
-    } 
-  },
-  watch: {
-    didAutoLogout(curValue, oldValue) {
-      if (curValue && curValue !== oldValue) {
-        this.$router.replace('/coaches');
-      }
+  setup() {
+    // const uName = ref('Maximilian');
+    const firstName = ref('');
+    const lastName = ref('');
+    const uAge = ref(31);
+    // const user = reactive({
+    //   name: 'Maximilian',
+    //   age: 31,
+    // });
+
+    const uName = computed(function() {
+      return firstName.value + ' ' + lastName.value;
+    });
+
+     watch([uAge, uName], function(newValues, oldValues) {
+      console.log('Old age: ' + oldValues[0]);
+      console.log('New age: ' + newValues[0]);
+      console.log('Old name: ' + oldValues[1]);
+      console.log('New name: ' + newValues[1]);
+    });
+
+    function setNewAge() {
+      uAge.value = 33;
     }
-  }
-}
+
+    return {
+      userName: uName,
+      age: uAge,
+      setAge: setNewAge,
+      firstName,
+      lastName
+    };
+  },
+  // data() {
+  //   return {
+  //     userName: 'Maximilian',
+  //     age: 31
+  //   };
+  // },
+  // methods: {
+  //   setNewAge() {
+  //     this.age = 32;
+  //   }
+  // },
+  // watch: {
+  //   age(val) {
+  //     console.log(val);
+  //   }
+  // }
+};
 </script>
 
 <style>
-@import url("https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap");
-
 * {
   box-sizing: border-box;
 }
 
 html {
-  font-family: "Roboto", sans-serif;
+  font-family: sans-serif;
 }
 
 body {
   margin: 0;
 }
 
-.route-enter-from {
-  opacity: 0;
-  transform: translateY(-30px)
-}
-
-.route-leave-to {
-  opacity: 0;
-  transform: translateY(30px)
-}
-
-.route-enter-active {
-  transition: all 0.3s ease-out;
-}
-
-.route-leave-active {
-  transition: all 0.3s ease-in;
-}
-
-.route-enter-to,
-.route-leave-from {
-  opacity: 1;
-  transform: translateY(0);
+.container {
+  margin: 3rem auto;
+  max-width: 30rem;
+  border-radius: 12px;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+  padding: 1rem;
+  text-align: center;
 }
 </style>
